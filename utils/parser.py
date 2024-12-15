9fe89fca734e0e594094ff57b4f7b92a7649c916
unique1i <clap4jack@gmail.com>
2024-12-15 14:32:38 +0100

    default num_workers to 16

--- utils/parser.py
+++ utils/parser.py
@@ -9,7 +9,7 @@ def get_args():
     parser.add_argument(
         '--launcher', choices=['none', 'pytorch'], default='none', help='job launcher')
     parser.add_argument('--local_rank', type=int, default=0)
-    parser.add_argument('--num_workers', type=int, default=8)
+    parser.add_argument('--num_workers', type=int, default=16)
     parser.add_argument('--seed', type=int, default=0, help='random seed')
     parser.add_argument('--exp_name', type=str,
                         default='default', help='experiment name')
